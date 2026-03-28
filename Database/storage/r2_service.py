27698d05466574b81ceb7049b24c4d7ad5c27c04
adityasahani001 <aditya.sahani.cs27@iilm.edu>
2026-03-28 15:44:49 +0530

    Added Keys and bucket of darabase

--- Database/storage/r2_service.py
+++ Database/storage/r2_service.py
@@ -1,17 +1,27 @@
 import boto3
 import os
+from dotenv import load_dotenv
 
+# Load .env
+load_dotenv()
+
+# Get values from .env
+R2_ENDPOINT = os.getenv("R2_ENDPOINT")
+R2_ACCESS_KEY = os.getenv("R2_ACCESS_KEY")
+R2_SECRET_KEY = os.getenv("R2_SECRET_KEY")
+R2_BUCKET = os.getenv("R2_BUCKET")
+
+# Create S3 client
 s3 = boto3.client(
     "s3",
-    endpoint_url="https://<account_id>.r2.cloudflarestorage.com",
-    aws_access_key_id="YOUR_R2_KEY",
-    aws_secret_access_key="YOUR_R2_SECRET"
+    endpoint_url=R2_ENDPOINT,
+    aws_access_key_id=R2_ACCESS_KEY,
+    aws_secret_access_key=R2_SECRET_KEY
 )
-
-BUCKET = "multiva"
 
 
 def upload_file(file_path, key):
-    s3.upload_file(file_path, BUCKET, key)
+    s3.upload_file(file_path, R2_BUCKET, key)
 
-    return f"https://<your-public-url>/{key}"
+    # Correct public URL
+    return f"{R2_ENDPOINT}/{R2_BUCKET}/{key}"
